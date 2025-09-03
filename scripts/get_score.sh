78e83fc8669a8347c56f42e5808b0e8d6b0d4f07
jianzhnie <jianzhnie@126.com>
2025-09-03 09:44:48 +0800

    Update llmeval

--- scripts/get_score.sh
+++ scripts/get_score.sh
@@ -18,22 +18,22 @@ mkdir -p "${reval_dir}"
 
 # --- Evaluate Each Task ---
 # Evaluate math500
-python ./eval/eval.py \
-    --input_path "${output_dir}/math500_bz${n_samples}.jsonl" \
-    --cache_path "${reval_dir}/math500_bz${n_samples}.jsonl" \
-    --task_name "math_opensource/math500" \
-    > "${reval_dir}/math500_bz${n_samples}_res_result.txt"
+# python ./llmeval/math_eval/eval.py \
+#     --input_path "${output_dir}/math500_bz${n_samples}.jsonl" \
+#     --cache_path "${reval_dir}/math500_bz${n_samples}.jsonl" \
+#     --task_name "math_opensource/math500" \
+#     > "${reval_dir}/math500_bz${n_samples}_res_result.txt"
 
 
 # Evaluate aime24
-python ./eval/eval.py \
+python ./llmeval/math_eval/eval.py \
     --input_path "${output_dir}/aime24_bz${n_samples}.jsonl" \
     --cache_path "${reval_dir}/aime24_bz${n_samples}.jsonl" \
     --task_name "math_opensource/aime24" \
     > "${reval_dir}/aime24_bz${n_samples}_res_result.txt"
 
 # Evaluate aime25
-python ./eval/eval.py \
+python ./llmeval/math_eval/eval.py \
     --input_path "${output_dir}/aime25_bz${n_samples}.jsonl" \
     --cache_path "${reval_dir}/aime25_bz${n_samples}.jsonl" \
     --task_name "math_opensource/aime25" \
