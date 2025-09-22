095f7762a288804f69b782c2bf53ce35ed204519
jianzhnie <jianzhnie@126.com>
2025-09-22 19:15:27 +0800

    Update compass_verifie

--- scripts/compass_verifier/offline_infer_3b.sh
+++ scripts/compass_verifier/offline_infer_3b.sh
@@ -8,11 +8,12 @@ input_file=/home/jianzhnie/llmtuner/llm/LLMEval/output/am-thinking-0528/infer_qw
 output_file=./output/CompassVerifier-3B/infer_qwen25_32B_math_top_30K_rl_merged_comass-verifier3b.jsonl
 model_name_or_path=/home/jianzhnie/llmtuner/hfhub/models/OpenCompass/CompassVerifier-3B
 
-python /home/jianzhnie/llmtuner/llm/LLMEval/llmeval/vllm/compassverifier_offline_infer.py \
+python /home/jianzhnie/llmtuner/llm/LLMEval/llmeval/vllm/verifier_offline_infer.py \
     --input_file $input_file \
     --output_file $output_file \
     --model_name_or_path $model_name_or_path \
     --trust_remote_code \
+    --verifier_prompt_type compassverify_prompt \
     --input_key prompt \
     --label_key answer \
     --response_key gen \
