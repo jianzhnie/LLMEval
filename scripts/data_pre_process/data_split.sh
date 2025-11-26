b0dc19ec96f6fb453fe0cf7838494872fbe939ea
jianzhnie <jianzhnie@126.com>
2025-11-26 17:56:35 +0800

    Update scripts

--- scripts/data_pre_process/data_split.sh
+++ scripts/data_pre_process/data_split.sh
@@ -1,5 +1,5 @@
 python /home/jianzhnie/llmtuner/llm/LLMEval/scripts/data_pre_process/data_split.py \
-  --input /home/jianzhnie/llmtuner/llm/LLMEval/data/HARP/HARP_filter.jsonl \
-  --output_dir /home/jianzhnie/llmtuner/llm/LLMEval/data/HARP/ \
-  --prefix HARP_shard \
-  --num_shards 32
+  --input /home/jianzhnie/llmtuner/llm/LLMEval/data/partial_right_problem.jsonl \
+  --output_dir /home/jianzhnie/llmtuner/llm/LLMEval/data/Nemotron-Post-Training-Dataset-v1 \
+  --prefix partial_right_problem_shard \
+  --num_shards 128
